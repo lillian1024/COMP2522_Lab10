54aed7fce323f7fe6728495c80a10a235f71760a
Blackearth2 <perraudamaury@yahoo.com>
2025-03-27 13:59:58 -0700

    creation

---
+++ src/code/Bank.java
@@ -0,0 +1,8 @@
+﻿/**
+ * Manages a collection of BankAccount objects, allowing the addition of
+ * new accounts and management through unique account IDs
+ * @author Amaury Perraud
+ */
+public class Bank
+{
+}
---
+++ src/code/BankAccount.java
@@ -0,0 +1,8 @@
+﻿/**
+ * : Manages a bank account with functionalities such as
+ * depositing, withdrawing, and balance enquiry
+ * @author Amaury Perraud
+ */
+public class BankAccount
+{
+}
